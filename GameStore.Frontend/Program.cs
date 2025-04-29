36180aff3c907339586406b86396149ea07f2249
Daisy Choi <chj15937@gmail.com>
2025-04-29 15:55:24 +1200

    add gameSummary model

--- GameStore.Frontend/Program.cs
+++ GameStore.Frontend/Program.cs
@@ -9,18 +9,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
+{ //if not in development env
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //thie app can only work with HTTPS
     app.UseHsts();
 }
+//thie app can only work with HTTPS
+// app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
-
+//protect our website 
 app.UseAntiforgery();
-
+//if its not dynaamic, supply as they are. static files(http, css etc)
 app.MapStaticAssets();
+//configure middleware, figure all of component from here
 app.MapRazorComponents<App>();
-
+//ready to service app
 app.Run();
